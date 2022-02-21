496d5a444a86638f80428c180066ed6667b078b0
jyyoun1022 <jyyoun1022@naver.com>
2022-02-21 15:04:38 +0900

    ModelAttribute의 다양한 사용방법

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -35,6 +32,51 @@ public class BasicItemController {
         model.addAttribute("item",item);
         return "basic/item";
     }
+    @GetMapping("/add")
+    public String addForm(){
+        return "/basic/addForm";
+    }
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model){
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item",item);
+
+        return "/basic/item";
+    }
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item")Item item){
+
+        itemRepository.save(item);
+//        model.addAttribute("item",item);
+
+        return "/basic/item";
+    }
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item){
+
+        itemRepository.save(item);
+//        model.addAttribute("item",item);
+
+        return "/basic/item";
+    }
+//     @PostMapping("/add")
+    public String addItemV4(Item item){
+
+        itemRepository.save(item);
+//        model.addAttribute("item",item);
+
+        return "/basic/item";
+    }
+
 
     /**
      * 테스트를 위하여 데이터를 추가
